ad76b77ef9a6e9e3067460259bad02aa4efe1102
Tu Vu <vuanhtu52@gmail.com>
2023-10-26 13:56:41 +0700

    Add factory functions Node and Tree

---
+++ balancedBST.js
@@ -0,0 +1,46 @@
+const Node = (value = null, leftChild = null, rightChild = null) => {
+    let _value = value;
+    let _leftChild = leftChild;
+    let _rightChild = rightChild;
+
+    const setValue = val => {
+        _value = val;
+    };
+
+    const getValue = () => {
+        return _value;
+    };
+
+    const setLeftChild = node => {
+        _leftChild = node;
+    };
+
+    const getLeftChild = () => {
+        return _leftChild;
+    };
+
+    const setRightChild = node => {
+        _rightChild = node;
+    };
+
+    const getRightChild = () => {
+        return _rightChild;
+    };
+
+    return {
+        setValue,
+        getValue,
+        setLeftChild,
+        getLeftChild,
+        setRightChild,
+        getRightChild,
+    };
+};
+
+const Tree = arr => {
+    let _root = null;
+
+    return {
+
+    };
+};
